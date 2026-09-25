43ac4e04cda0b64be5bcfe3de59d81d3e2de0cbe
Ishan Ghosh <superworldig@gmail.com>
2026-09-25 22:53:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.09%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.c
@@ -0,0 +1,10 @@
+bool isPerfectSquare(int num) {
+    if (num == 1) return true;
+
+    for (long i = 1; i * i <= num; i++) {
+        if (i * i == num) {
+            return true;
+        }
+    }
+    return false;
+}
